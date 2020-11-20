5449057e5d2eab97b0c5308ca0f29598dc945fd2
CharlotteSaury <saury.charlotte@wanadoo.fr>
2020-11-20 15:39:22 +0000

    create responsive nav

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,31 @@
 $(function () {
+
   $(document).scroll(function () {
     var $nav = $(".nav");
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
   });
+
+  var content = $('.top-nav');
+  var sidebar = $('#nav-burger-sidebar');
+
+  sidebar.html(content.html());
+
+  $('#nav-burger-button').click(function (e) {
+    e.preventDefault();
+    $('#nav-burger-sidebar').css('transform', 'translateX(0)');
+    $('#nav-overlay').css('display', 'block');
+  });
+
+  $('#nav-overlay').click(function(e) {
+    e.preventDefault();
+    $('#nav-burger-sidebar').css('transform', 'translateX(-100%)');
+    $('#nav-overlay').css('display', 'none');
+  });
+
+  $('#nav-burger-sidebar a').click(function(e) {
+    $('#nav-burger-sidebar').css('transform', 'translateX(-100%)');
+    $('#nav-overlay').css('display', 'none');
+  });
+
+
 });
